566c804394376e5c75b8102710af1b272a6b7123
Shree Varshan G <shreevarshang2003@gmail.com>
2024-03-05 10:28:23 +0530

    added files backend

---
+++ backend/day3/UserService.java
@@ -0,0 +1,18 @@
+package com.webzy.jwt.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.webzy.jwt.entity.AppUser;
+import com.webzy.jwt.entity.Plan;
+
+public interface UserService {
+	
+	ResponseEntity<?> registerNewUser(AppUser user);
+
+	AppUser findUserNameByEmail(String email);
+
+	List<Plan> findPlansByOperator(String operatorName);
+
+}
